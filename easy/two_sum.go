2f480cb428db76667fd29530c7f84f40a780374f
sselvaganesh <me.selvaganesh@gmail.com>
2022-12-26 15:18:35 -0800

    two sum - approach 2

--- easy/two_sum.go
+++ easy/two_sum.go
@@ -1,12 +1,10 @@
 package easy
 
 
-func TwoSum(nums []int, target int) []int {
-	return twoSum1(nums, target)
-}
+func TwoSum1(nums []int, target int) []int {
 
-
-func twoSum1(nums []int, target int) []int {
+	//Time Complexity: O(n^2)
+	//Space Complexity: O(1) - No additional space
 
 	var result []int
 	var ok bool
@@ -29,5 +27,64 @@ func twoSum1(nums []int, target int) []int {
 
 	return result
 }
+
+
+func TwoSum2(nums []int, target int) []int {
+	
+	var result []int
+	inpMap := make(map[int][]int)
+
+	// Time Complexity: O(n) 
+	// Space Complexity: O(n)
+	for i:=0; i<len(nums); i++ {
+		
+		val, ok := inpMap[nums[i]]
+		if ok {
+			if (nums[i]*2 == target) {
+				result = append(result, val[0])
+				result = append(result, i)
+				break
+			}		
+		} else {		
+			val, ok := inpMap[target-nums[i]]
+			if ok {
+				result = append(result, val[0])
+				result = append(result, i)
+				break
+			}		
+		}
+		
+		val = append(val, i)
+		inpMap[nums[i]] = val		
+	}
+
+	return result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- easy/two_sum_test.go
+++ easy/two_sum_test.go
@@ -4,18 +4,23 @@ import (
 	"testing"
 )
 
+type testcase struct {
+	testId int
+	input []int
+	target int
+	expected []int
+}
+var tests = []testcase {
+	{ testId: 1, input: []int{2,7,11,15}, target: 9, expected: []int{0,1}, }, 
+	{ testId: 2, input: []int{3,2,4}, target: 6, expected: []int{1,2}, }, 
+	{ testId: 3, input: []int{3,3}, target: 6, expected: []int{0,1}, }, 		
+}
 
-func TestTwoSum(t *testing.T){
 
-	type testcase struct {
-		testId int
-		input []int
-		target int
-		expected []int
-	}
+func TestTwoSum1(t *testing.T){
 
 	runtest := func(tc testcase) {		
-		result := TwoSum(tc.input, tc.target)
+		result := TwoSum1(tc.input, tc.target)
 		if result == nil {
 			t.Fatalf("Failed: [%+v]. Output is nil", tc)
 		} else if (len(result) < 2) {
@@ -26,15 +31,31 @@ func TestTwoSum(t *testing.T){
 			t.Logf("Passed: [%+v]", tc)
 		}
 	}
-
-	tests := []testcase {
-		{ testId: 1, input: []int{2,7,11,15}, target: 9, expected: []int{0,1}, }, 
-		{ testId: 2, input: []int{3,2,4}, target: 6, expected: []int{1,2}, }, 
-		{ testId: 3, input: []int{3,3}, target: 6, expected: []int{0,1}, }, 
-		
-		}
 	
 	for _, testObj := range tests {
 		runtest(testObj)
 	}
 }
+
+
+func TestTwoSum2(t *testing.T){
+
+	runtest := func(tc testcase) {		
+		result := TwoSum2(tc.input, tc.target)
+		if result == nil {
+			t.Fatalf("Failed: [%+v]. Output is nil", tc)
+		} else if (len(result) < 2) {
+			t.Fatalf("Failed: [%+v]. Output is not a 2 element array", tc)
+		} else if (result[0] != tc.expected[0] || result[1] != tc.expected[1]) {
+			t.Fatalf("Failed: [%+v]. Output [%v]", tc, result)
+		} else {
+			t.Logf("Passed: [%+v]", tc)
+		}
+	}
+	
+	for _, testObj := range tests {
+		runtest(testObj)
+	}
+}
+
+
