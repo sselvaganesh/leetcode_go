9c77c690d2b0adc70af8aa5a927872440f5bf046
sselvaganesh <me.selvaganesh@gmail.com>
2022-12-30 21:20:49 -0800

    string reverse

---
+++ easy/string_reverse_test.go
@@ -0,0 +1,36 @@
+package easy
+
+import(
+	"testing"
+)
+
+
+func TestStringReverse(t *testing.T){
+
+	type testcase struct {
+		testId uint8
+		inp string
+		rev string
+	}
+
+	runtest := func(tc testcase) {		
+		if rev:=StringReverse(tc.inp); rev != tc.rev {
+			t.Fatalf("TestCase [%+v] Actual [%s]", tc, rev)
+		}
+	}
+
+	tests := []testcase{
+		{testId: 1, inp: "selva", rev: "avles"},
+		{testId: 2, inp: "hsenag", rev: "ganesh"},
+		{testId: 3, inp: "", rev: ""},
+		{testId: 4, inp: "1234", rev: "4321"},
+		{testId: 5, inp: "golang", rev: "gnalog"},
+		{testId: 6, inp: "stack", rev: "kcats"},		
+	}
+	
+	for _, test := range tests {
+		runtest(test)
+	}
+
+
+}
