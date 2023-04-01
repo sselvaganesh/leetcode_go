00f9569d02e66249d678dfee1c9e66b38f7c4efb
sselvaganesh <me.selvaganesh@gmail.com>
2023-04-01 14:07:23 -0700

    Leetcode - code75

---
+++ code75/levelone/day6/n-ary_tree_preorder_traversal.go
@@ -0,0 +1,55 @@
+package tree
+
+/**
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+    Val int
+    Children []*Node
+}
+
+
+func Preorder(root *Node) []int {
+    
+    if root==nil{
+        return nil
+    }
+
+    return traverse(root)
+    
+}
+
+func traverse(curNode *Node) []int {
+
+    if curNode==nil {
+        return nil
+    }
+
+    var result []int
+    result = append(result, curNode.Val)
+
+    for _, node := range curNode.Children {
+        temp := traverse(node)
+        if temp!=nil{
+            result=append(result, temp...)
+        }
+    }
+
+    return result
+}
+
+
